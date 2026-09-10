0e3318996d142f27600ccae7d07840c6d9c2b20c
xiaociwei01 <xiaolu200808@gmail.com>
2026-09-11 05:41:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-/* CiweiPet Service Worker v2 */
-var CACHE_NAME = 'ciweipet-v2';
+/* ============================================================
+   CiweiPet Service Worker · v3
+   缓存：图标 + 7 张刺猬图片 + 页面
+================================================================ */
+var CACHE_NAME = 'ciweipet-v3';
 var STATIC_ASSETS = [
     './',
     './index.html',
     './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
     './idle-1.png',
     './idle-2.png',
     './blink.png',
